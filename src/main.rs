eefd688c39b4cbf85dc753f56fbde185e0fe6bdd
Václav Blažej <blazeva1@fit.cvut.cz>
2020-06-07 19:43:59 +0200

    update to 2018

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use rand::Rng;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let n:i32 = rng.gen();
+    let m = ((n%100)+100)%100;
+    println!("{}", m);
+    let mut guess = -1;
+    while guess != m {
+        print!("guess: ");
+        if guess > m {
+            println!("lower");
+        } else if guess < m {
+            println!("higher");
+        }
+
+        let mut input = Input::Standard(io::stdin());
+        guess = input!();
+    }
+    println!("Correct!");
 }
