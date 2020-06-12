0138fb805e88d7c022afb1d5e64d317305a1bf2d
Václav Blažej <blazeva1@fit.cvut.cz>
2020-06-12 10:27:16 +0200

    Check out primes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
-use rand::Rng;
-use std::io::{self, Read};
+
+fn is_prime(value: i64) -> bool {
+    let mut i=2;
+    while i*i <= value {
+        if value % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    return true;
+}
+
+fn chain2(value: i64) -> Vec<i64> {
+    let mut val = value;
+    if val == 1 {
+        return vec!(1);
+    }
+    let mut res;
+    if val % 2 == 0 {
+        res = chain2(value/2);
+    } else {
+        res = chain2(value*3+1);
+    }
+    res.push(value);
+    return res;
+}
+
+fn chain(value: i64) -> Vec<i64> {
+    let mut val = value;
+    let mut res = vec!();
+    loop {
+        res.push(val);
+        if val == 1 {
+            break;
+        }
+        if val % 2 == 0 {
+            val = val / 2;
+        } else {
+            val = val * 3 + 1;
+        }
+    }
+    return res;
+}
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let n:i32 = rng.gen();
-    let m = ((n%100)+100)%100;
-    println!("{}", m);
-    let mut guess = -1;
-    while guess != m {
-        print!("guess: ");
-        if guess > m {
-            println!("lower");
-        } else if guess < m {
-            println!("higher");
-        }
-
-        let mut input = Input::Standard(io::stdin());
-        guess = input!();
+    let to = 10;
+    for i in 1..=to {
+        println!("{}: {:?}", i, chain2(i));
     }
-    println!("Correct!");
+    // for i in 1..=to {
+    //     println!("{}: {:?}", i, is_prime(i));
+    // }
 }
